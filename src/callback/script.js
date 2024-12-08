ed127d6cb64706939f7d2c71375f859c131bd31c
axelquartz <axelarep@gmail.com>
2024-12-08 15:03:27 -0600

    Promise.all

--- src/callback/script.js
+++ src/callback/script.js
@@ -111,15 +111,86 @@
 //   .then((data) => console.log(data[1]))
 //   .catch((err) => console.log(err));
 
-const person = {
-  name: "Axel",
-};
+// const person = {
+//   name: "Axel",
+// };
 
-person.sayHello = function () {
-  console.log(`Hello, my name is ${this.name}`);
-};
+// person.sayHello = function () {
+//   console.log(`Hello, my name is ${this.name}`);
+// };
 
-person.sayHello();
+// person.sayHello();
 
-const salutate = person.sayHello.bind({ name: "Rerik" });
-salutate();
+// const salutate = person.sayHello.bind({ name: "Rerik" });
+// salutate();
+
+// async function fetchApi() {
+//   try {
+//     const url = await fetch("https://api.escuelajs.co/api/v1/products");
+//     const data = await url.json();
+//     console.log([data[0]]);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// fetchApi();
+
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const validation = true;
+//       if (validation) {
+//         resolve(fetch("https://api.escuelajs.co/api/v1/products"));
+//       } else {
+//         reject("Error fetching API");
+//       }
+//     }, 4000);
+//   });
+// }
+
+// // fetchData()
+// //   .then((data) => console.log(data))
+// //   .catch((err) => console.log(err));
+
+// async function fetchFinalData() {
+//   try {
+//     console.log("Fetching Final User Data...");
+//     const data = await fetchData();
+//     const result = await data.json();
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// fetchFinalData();
+
+function fetchProductData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fetch("https://api.escuelajs.co/api/v1/products"));
+    }, 2000);
+  });
+}
+
+function fetchMoreData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fetch("https://api.escuelajs.co/api/v1/products"));
+    }, 4000);
+  });
+}
+
+async function getAllData() {
+  try {
+    console.log("fetching data");
+    const allData = await Promise.all([fetchProductData(), fetchMoreData()]);
+    const jsonData = await Promise.all(allData.map((response) => response.json()));
+    console.log(jsonData);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getAllData();
